0c00d7c5af0d37b5eb120a2fbf1639d4b21c4e24
Alexey Sysoev <wowregfatgod@yandex.ru>
2022-10-26 19:14:09 +0300

    first working test

--- src/main/java/PageObject/MainPage.java
+++ src/main/java/PageObject/MainPage.java
@@ -5,10 +5,13 @@ import org.openqa.selenium.WebDriver;
 
 public class MainPage {
     private WebDriver driver;
+
+    //URL
+    private final String url = "https://qa-scooter.praktikum-services.ru/";
     //кнопка "Заказать" в хедере
-    private final By orderHeaderButton = By.className("Button_Button__ra12g");
+    private final By orderHeaderButton = By.xpath(".//Button[@class='Button_Button__ra12g']");
     //кнопка "Заказать" в контенте
-    private final By orderBodyButton = By.className("Button_Button__ra12g Button_Middle__1CSJM");
+    private final By orderBodyButton = By.xpath(".//Button[contains(@class, 'Button_Middle__1CSJM')]");
     //элемент списка "Вопросы о важном" - заготовка (accordion__heading-0)
     private final String liAboutImportante ="accordion__heading-";
     //ответ на вопрос списка "Вопросы о важном" - заготовка (accordion__panel-0)
@@ -18,19 +21,24 @@ public class MainPage {
     public MainPage (WebDriver driver){
         this.driver = driver;
     }
+    //
+    public void open(){
+        driver.get(url);
+    }
     //клик по кнопке "Заказать" в хедере
-    public void orderHeaderButtonClick(By orderHeaderButton){
+    public void orderHeaderButtonClick(){
         driver.findElement(orderHeaderButton).click();
     }
     //клик по кнопке "Заказать" в контенте
-    public void orderBodyButtonClick(By orderBodyButton){
+    public void orderBodyButtonClick(){
         driver.findElement(orderBodyButton).click();
     }
     //клик по элементу списка "Вопросы о важном"
     public void liAboutImportanteClick(String liNumber) {
         driver.findElement(By.id(liAboutImportante+liNumber)).click();
     }
-    public void liAnswerPanelGetText (String AnswerNumber){
-        driver.findElement(By.id(liAnswerPanelText+AnswerNumber)).getText();
+    //получение текста с ответом
+    public String liAnswerPanelGetText (String AnswerNumber){
+        return driver.findElement(By.id(liAnswerPanelText+AnswerNumber)).getText();
     }
 }
--- src/test/java/UiTests/AboutImportantListTests.java
+++ src/test/java/UiTests/AboutImportantListTests.java
@@ -1,24 +1,53 @@
 package UiTests;
-//import org.junit.Before;
+import org.hamcrest.MatcherAssert;
+import static org.hamcrest.CoreMatchers.containsString;
+//import org.junit.Assert;
+import org.junit.Assert;
+import org.junit.Before;
+import PageObject.MainPage;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.WebElement;
-//import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.junit.After;
 import org.junit.Test;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.JavascriptExecutor;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.time.Duration;
+//import java.util.regex.Matcher;
 
 
 public class AboutImportantListTests {
-
-    WebDriver driver = new FirefoxDriver();
-
-
-  //  @Before
-
+    WebDriver driver;
+    String expected;
+    String result;
+    @Before
+    public void preSettings(){
+        System.setProperty("webdriver.chrome.driver","C:/WebDriver/bin/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+    }
 
     @Test
-    public void CheckAboutImportantListPanelText(){
-        driver.get("https://qa-scooter.praktikum-services.ru/");
+    public void checkAboutImportantListPanelText(){
+        //driver.get("https://qa-scooter.praktikum-services.ru/");
+        MainPage mainPage = new MainPage(driver);
+        mainPage.open();
+        WebElement element = driver.findElement(By.id("accordion__heading-0"));
+        ((JavascriptExecutor) driver)
+                .executeScript(
+                        "arguments[0].scrollIntoView();",
+                        element
+                );
+
+        //mainPage.liAboutImportanteClick("0");
+        driver.findElement(By.xpath(".//div[@id='accordion__heading-0']")).click();
+        expected = "Сутки — 400 рублей. Оплата курьеру — наличными или картой.";
+        result = driver.findElement(By.xpath(".//div[@id='accordion__panel-0']/p")).getText();
+        Assert.assertEquals(expected, result);
+
+
 
     }
     @After
